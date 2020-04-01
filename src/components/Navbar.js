a75fa3618b1853706806a97043a72c66bb899df3
apc <alexcurran.va@gmail.com>
2020-04-01 17:38:47 -0500

    Added basic styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,9 +6,9 @@ function Navbar() {
         <nav className="nav">
             <a href="" className="nav__logo">Web Dev News</a>
             <ul className="nav__list">
-                <li className="nav__item"><Link to="/">Home</Link></li>
-                <li className="nav__item"><Link to="/about">About</Link></li>
-                <li className="nav__item"><Link to="/contact">Contact</Link></li>
+                <li className="nav__item"><Link className="nav__link" to="/">Home</Link></li>
+                <li className="nav__item"><Link className="nav__link" to="/about">About</Link></li>
+                <li className="nav__item"><Link className="nav__link" to="/contact">Contact</Link></li>
             </ul>
         </nav>
     );
